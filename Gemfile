d529cc95bf0a8c61dc3948ae06fbdad30829096c
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-03-21 16:22:51 +0900

    add mallinfo by _ko1

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem "fluentd", "= 0.10.32"
 gem "jemalloc", "= 0.1.9"
 gem "msgpack", "= 0.4.7"
 gem "fluent-plugin-flowcounter"
+gem "mallinfo", :git => "https://github.com/ko1/mallinfo"
+
--- dumpstat.rb
+++ dumpstat.rb
@@ -1,11 +1,23 @@
-def dump_process_stat
-  {count_objects: ObjectSpace.count_objects, gc_stat: GC.stat}
+require 'objspace'
+require 'mallinfo'
+
+def dump_process_stat h
+  # {count_objects: ObjectSpace.count_objects, gc_stat: GC.stat}
+  GC.start
+  GC.stat(h)
+  "#{h[:total_allocated_object]}\t#{h[:total_freed_object]}\t#{h[:heap_used]}\t#{h[:heap_length]}\t" + 
+  "#{ObjectSpace.memsize_of_all()}\t" +
+  #"#{GC.malloc_allocated_size}\t" +
+  "#{ObjectSpace.mallinfo.values.join("\t")}\t" + 
+  "#{File.read("/proc/self/statm").split(/\s+/).join("\t")}"
 end
 
 require 'pp'
 Thread.new do
+  Thread.current.abort_on_exception = true
+  h = {}
   while true
-    pp dump_process_stat
-    sleep 10
+    puts dump_process_stat(h); STDOUT.flush
+    sleep 1
   end
 end
